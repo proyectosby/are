3e15c386961d8ed7accf9f665e3e4d960e2c2e9f
Oscar Lopez <oscar.lopez@byallgraphic.com>
2018-03-01 18:30:38 -0500

    cambio prueba
    cambio prueba

--- config/db.php
+++ config/db.php
@@ -1,18 +1,19 @@
 <?php
 
+
 return [
-    'class' 	=> 'yii\db\Connection',
-	'dsn' 		=> 'pgsql:host=192.168.10.7;port=5432;dbname=sga_aprender',
-	'username' 	=> 'usr_aprender',
-	'password' 	=> 'A123456a',
-	'charset' 	=> 'utf8',
-
-
-	// 'class' 	=> 'yii\db\Connection',
-	// 'dsn' 		=> 'pgsql:host=localhost;port=5432;dbname=sga_aprender',
-	// 'username' 	=> 'postgres',
-	// 'password' 	=> 'root',
+    // 'class' 	=> 'yii\db\Connection',
+	// 'dsn' 		=> 'pgsql:host=192.168.10.7;port=5432;dbname=sga_aprender',
+	// 'username' 	=> 'usr_aprender',
+	// 'password' 	=> 'A123456a',
 	// 'charset' 	=> 'utf8',
+
+
+	'class' 	=> 'yii\db\Connection',
+	'dsn' 		=> 'pgsql:host=localhost;port=5432;dbname=sga_aprender',
+	'username' 	=> 'postgres',
+	'password' 	=> 'root',
+	'charset' 	=> 'utf8',
 	
 	
 	
