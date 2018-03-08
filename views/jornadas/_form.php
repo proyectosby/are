b67f1dc42ddaf9207825bc1998994fee617a8d94
Edwin Molina <36931573+emolinagrisales@users.noreply.github.com>
2018-03-07 23:46:46 -0500

    Se agrea place holder

--- views/jornadas/_form.php
+++ views/jornadas/_form.php
@@ -12,7 +12,7 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'descripcion')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'descripcion')->textInput(['maxlength' => true, 'placeHolder' => 'Digite la jornada' ]) ?>
 
     <div class="form-group">
         <?= Html::submitButton('Guardar', ['class' => 'btn btn-success']) ?>
