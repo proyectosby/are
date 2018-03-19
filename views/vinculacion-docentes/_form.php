15f1939a3edd58e1ad2f3e2406f0df2935c93e81
Edwin Molina <36931573+emolinagrisales@users.noreply.github.com>
2018-03-19 11:05:45 -0500

    Se cambia save por guardar

--- views/vinculacion-docentes/_form.php
+++ views/vinculacion-docentes/_form.php
@@ -36,7 +36,7 @@ use dosamigos\datepicker\DatePicker;
     <?= $form->field($model, 'estado')->dropDownList( $estados ) ?>
 
     <div class="form-group">
-        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+        <?= Html::submitButton('Guardar', ['class' => 'btn btn-success']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
