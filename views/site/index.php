02a8227220e36f329be784ea05d67cc11fa60f5d
Juan Segura <36551418+derpickel@users.noreply.github.com>
2018-04-09 16:40:59 -0500

    Add files via upload
    Carga de logo MCEE y modificación del archivo index

--- views/site/index.php
+++ views/site/index.php
@@ -9,5 +9,6 @@ $this->title = 'Sistema de Información MCEE';
 
     <div class="jumbotron">
         <h2>Bienvenido al sistema de información MI COMUNIDAD ES ESCUELA</h2>
+        <img src="../views/site/logo_mcee.png" style="wodth: 75%; margin: 15px auto;">
 	</div>
 </div>
