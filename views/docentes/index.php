7c1ed66edf864d59acbe8d8de8dd423fc6dc2f41
vivianaRodas <vivianaRodas>
2018-04-09 13:09:25 -0500

    data tables a docentes
    data tables a docentes

--- views/docentes/index.php
+++ views/docentes/index.php
@@ -30,9 +30,42 @@ $this->params['breadcrumbs'][] = $this->title;
         <?= Html::a('Agregar', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
-    <?= GridView::widget([
+    <?= DataTables::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
+		'clientOptions' => [
+		'language'=>[
+                'url' => '//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Spanish.json',
+            ],
+		"lengthMenu"=> [[20,-1], [20,Yii::t('app',"All")]],
+		"info"=>false,
+		"responsive"=>true,
+		 "dom"=> 'lfTrtip',
+		 "tableTools"=>[
+			 "aButtons"=> [  
+				// [
+				// "sExtends"=> "copy",
+				// "sButtonText"=> Yii::t('app',"Copiar")
+				// ],
+				// [
+				// "sExtends"=> "csv",
+				// "sButtonText"=> Yii::t('app',"CSV")
+				// ],
+				[
+				"sExtends"=> "xls",
+				"oSelectorOpts"=> ["page"=> 'current']
+				],
+				[
+				"sExtends"=> "pdf",
+				"sButtonText"=> Yii::t('app',"PDF")
+				],
+				// [
+				// "sExtends"=> "print",
+				// "sButtonText"=> Yii::t('app',"Imprimir")
+				// ],
+			],
+		 ],
+	],
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
 
