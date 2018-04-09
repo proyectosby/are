cd17a730c825c5fb5c72bbb02472dc7baab1ca2b
vivianaRodas <vivianaRodas>
2018-04-09 14:41:03 -0500

    Cambios index nombre proyecto
    Cambios index nombre proyecto

--- views/site/index.php
+++ views/site/index.php
@@ -2,12 +2,12 @@
 
 /* @var $this yii\web\View */
 
-$this->title = 'Sistema ARE';
+$this->title = 'Sistema de Información MCEE';
 ?>
 
 <div class="site-index">
 
     <div class="jumbotron">
-        <h2>Bienvenido al sistema ARE</h2>
+        <h2>Bienvenido al sistema de información MI COMUNIDAD ES ESCUELA</h2>
 	</div>
 </div>
--- views/site/index_ARE.php
+++ views/site/index_ARE.php
@@ -2,12 +2,12 @@
 
 /* @var $this yii\web\View */
 
-$this->title = 'Sistema MCEE';
+$this->title = 'Sistema ARE';
 ?>
 
 <div class="site-index">
 
     <div class="jumbotron">
-        <h2>Bienvenido al sistema MI COMUNIDAD ES ESCUELA</h2>
+        <h2>Bienvenido al sistema ARE</h2>
 	</div>
 </div>
