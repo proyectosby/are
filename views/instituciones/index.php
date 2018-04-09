ebc1a0c14df96ba513aeef527bb742ce0d1e4a2f
vivianaRodas <vivianaRodas>
2018-04-09 12:24:02 -0500

    cambio instituciones
    cambio instituciones

--- views/instituciones/index.php
+++ views/instituciones/index.php
@@ -11,6 +11,10 @@ Fecha: 09-03-2018
 Persona encargada: Edwin Molina Grisales
 Cambios Se agrega buscador
 ---------------------------------------
+Fecha: 05-04-2018
+Persona encargada: Viviana Rodas
+Cambios realizados: Se agregan los datatabes
+---------------------------------------
 **********/
 
 use yii\helpers\Html;
@@ -21,6 +25,7 @@ use app\models\Estados;
 use app\models\Sectores;
 use app\models\TiposInstituciones;
 use yii\helpers\ArrayHelper;
+use fedemotta\datatables\DataTables;
 
 /* @var $this yii\web\View */
 /* @var $dataProvider yii\data\ActiveDataProvider */
@@ -36,9 +41,42 @@ $this->params['breadcrumbs'][] = $this->title;
         <?= Html::a('Agregar', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
-    <?= GridView::widget([
+    <?= DataTables::widget([
         'dataProvider' => $dataProvider,
         'filterModel'  => $searchModel,
+		'clientOptions' => [
+		'language'=>[
+                'url' => '//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Spanish.json',
+            ],
+		"lengthMenu"=> [[20,-1], [20,Yii::t('app',"All")]],
+		"info"=>false,
+		"responsive"=>true,
+		 "dom"=> 'lfTrtip',
+		 "tableTools"=>[
+			 "aButtons"=> [  
+				// [
+				// "sExtends"=> "copy",
+				// "sButtonText"=> Yii::t('app',"Copiar")
+				// ],
+				// [
+				// "sExtends"=> "csv",
+				// "sButtonText"=> Yii::t('app',"CSV")
+				// ],
+				[
+				"sExtends"=> "xls",
+				"oSelectorOpts"=> ["page"=> 'current']
+				],
+				[
+				"sExtends"=> "pdf",
+				"sButtonText"=> Yii::t('app',"PDF")
+				],
+				// [
+				// "sExtends"=> "print",
+				// "sButtonText"=> Yii::t('app',"Imprimir")
+				// ],
+			],
+		 ],
+	],
         'columns' 	   => [
             ['class' => 'yii\grid\SerialColumn'],
             'descripcion',
