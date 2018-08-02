a78fffca956dfa232c4569ff40cee1b98e031cfe
vivianaRodas <vivianaRodas>
2018-08-02 13:31:05 -0500

    se agregan textos de calificación
    se agregan textos de calificación

--- views/dimension-opciones-seguimiento-docente/itemDimension.php
+++ views/dimension-opciones-seguimiento-docente/itemDimension.php
@@ -24,7 +24,16 @@ $form = ActiveForm::begin([
 			// ],
 		// ],
 	]);
-			
+		if( $dimension == 8 ){echo "<div><h4>Dimensión del SER</h4><br>
+        A continuación encontrará una serie de afirmaciones sobre aspectos relacionados con el SER del Docente-Tutor; califique de 1 a 4 siendo 1 Nunca y 4 Siempre, de acuerdo a la frecuencia en que usted considera que se evidencian estas características.<br> </div>   ";}	
+
+		if( $dimension == 9 ){echo "<div><h4>Dimensión del HACER</h4><br>
+        A continuación encontrará una serie de afirmaciones sobre aspectos relacionados con el HACER del Docente-Tutor; califique de 1 a 4 siendo 1 Nunca y 4 Siempre, de acuerdo a la frecuencia en que usted considera que se evidencian estas características.<br> </div>   ";}	
+
+		if( $dimension == 10 ){echo "<div><h4>Dimensión del SABER</h4><br>
+        A continuación encontrará una serie de afirmaciones sobre aspectos relacionados con el SABER del Docente-Tutor; califique de 1 a 4 siendo 1 Nunca y 4 Siempre, de acuerdo a la frecuencia en que usted considera que se evidencian estas características.<br> </div>   ";}	
+
+		
 foreach( $data as $key => $value ){
 	// echo "<div class='row'>";
 	echo $form->field( $value , 'descripcion' )->radioList( $parametros )->label(  $value->descripcion );
