fc440a25db9835001506dfb87869fd7f40d1df41
Viviana Rodas <viviana.rodas@byallgraphic.com>
2018-08-02 20:41:13 -0500

    cambio index
    cambio index

--- views/site/index.php
+++ views/site/index.php
@@ -1,4 +1,14 @@
 <?php
+if(@$_SESSION['sesion']=="si")
+{ 
+	// echo $_SESSION['nombre'];
+} 
+//si no tiene sesion se redirecciona al login
+else
+{
+	echo "<script> window.location=\"index.php?r=site%2Flogin\";</script>";
+	die;
+}
 
 //con que institucion y que sede se debe trabajar
 if (@$_GET['instituciones'])
